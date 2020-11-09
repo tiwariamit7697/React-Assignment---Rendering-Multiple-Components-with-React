a2a90425bd48699a1e7574eb21721468bbec1582
Amit <amitkumartiwari527@gmaial.com>
2020-11-09 20:29:16 +0530

    changed

---
+++ src/components/Description/Description.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import './Description.css';
+
+export default class Description extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+  return (
+    <h1>{this.props.data-ns-test}<h1/>
+    
+  );
+}
+
+
---
+++ src/components/Project/Project.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import './project.css';
+
+export default class project extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+  return (
+    <h1>{this.props.data-ns-test}<h1/>
+    
+  );
+}
+
+
