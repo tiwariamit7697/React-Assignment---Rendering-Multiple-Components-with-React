505262a287d47ae6c5d553ea7a3cf86186ccc6c9
Amit <amitkumartiwari527@gmaial.com>
2020-11-10 15:41:17 +0530

    changed

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => 
+{
+  return (
+    <h1 data-ns-test="project-name">"project name"</h1>
+    <h2 data-ns-test="project-description">"project description"</h2>
+    
+  )
+}
+
+
+export default App;
