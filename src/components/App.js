d6787dd85264e202721d8fc8067a9a2ae36b2d74
Amit <amitkumartiwari527@gmaial.com>
2020-11-10 15:54:20 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,10 @@ import '../styles/App.css';
 const App = () => 
 {
   return (
+    <div>
     <h1 data-ns-test="project-name">"project name"</h1>
     <h2 data-ns-test="project-description">"project description"</h2>
+    </div>
     
   )
 }
